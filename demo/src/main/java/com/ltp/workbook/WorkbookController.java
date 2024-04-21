fe36d3ef93907687ca907092f2c4e6eda3eace46
moffd234 <moffd234@gmail.com>
2024-04-21 12:45:09 -0400

    Made show class

--- demo/src/main/java/com/ltp/workbook/WorkbookController.java
+++ demo/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -8,6 +8,12 @@ public class WorkbookController {
     
     @GetMapping("/")
     public String getShows() {
+        Show bb = new Show("Breaking Bad", "Ozymandias", 10.0);
+        Show AoTH = new Show("Attack on Titan", "Hero", 9.9);
+        Show AoTP = new Show("Attack on Titan", "Perfect Game", 9.9);
+        Show sW = new Show("Star Wars: The Clone Wars", "Victory and Death", 9.9);
+        Show mR = new Show("Mr. Robot", "407 Proxy Authentication Required", 9.9);
+
         return "shows";
     }
 
