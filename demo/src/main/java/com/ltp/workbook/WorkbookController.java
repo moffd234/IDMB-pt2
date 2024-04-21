a20618d32a6c990442ef8b9f2c69a4815a22f2ff
moffd234 <moffd234@gmail.com>
2024-04-21 13:00:06 -0400

    named the attributes in getShows

--- demo/src/main/java/com/ltp/workbook/WorkbookController.java
+++ demo/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -1,19 +1,29 @@
 package com.ltp.workbook;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WorkbookController {
     
     @GetMapping("/")
-    public String getShows() {
+    public String getShows(Model model) {
         Show bb = new Show("Breaking Bad", "Ozymandias", 10.0);
-        Show AoTH = new Show("Attack on Titan", "Hero", 9.9);
-        Show AoTP = new Show("Attack on Titan", "Perfect Game", 9.9);
-        Show sW = new Show("Star Wars: The Clone Wars", "Victory and Death", 9.9);
-        Show mR = new Show("Mr. Robot", "407 Proxy Authentication Required", 9.9);
+        model.addAttribute("bb", bb);
 
+
+        Show AoTH = new Show("Attack on Titan", "Hero", 9.9);
+        model.addAttribute("AoTH", AoTH);
+
+        Show AoTP = new Show("Attack on Titan", "Perfect Game", 9.9);
+        model.addAttribute("AoTP", AoTP);
+
+        Show sw = new Show("Star Wars: The Clone Wars", "Victory and Death", 9.9);
+        model.addAttribute("sw", sw);
+
+        Show mr = new Show("Mr. Robot", "407 Proxy Authentication Required", 9.9);
+        model.addAttribute("mr", mr);
         return "shows";
     }
 
