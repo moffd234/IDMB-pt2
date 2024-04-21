24ba5e4d083c61588e72b94240e57e88572ebb33
moffd234 <moffd234@gmail.com>
2024-04-21 12:38:43 -0400

    Found starter project and added files

--- demo/src/main/java/com/ltp/workbook/WorkbookApplication.java
+++ demo/src/main/java/com/ltp/workbook/WorkbookApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.ltp.workbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class WorkbookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(WorkbookApplication.class, args);
 	}
 
 }
--- demo/src/test/java/com/ltp/workbook/WorkbookApplicationTests.java
+++ demo/src/test/java/com/ltp/workbook/WorkbookApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.ltp.workbook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class WorkbookApplicationTests {
 
 	@Test
 	void contextLoads() {
